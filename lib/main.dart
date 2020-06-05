6bbd2a7971b5747738fdd229db504a4a947d1041
claylton <clayltonsp@hotmail.com>
2020-06-05 20:23:57 -0300

    Criando a Lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -20,19 +22,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+//Stateful pos o estado da aplicação muda constantemente
+class HomePage extends StatefulWidget {
+  //Classe pai -> Os métodos criados abaixo só serão chamados uma vez
+
+  //Instanciando o método item -> É Tipo Lista Item
+  var items = new List<Item>();
+
+  //Construtor
+  HomePage() {
+    //inicializando a lista de items
+    items = [];
+    //Adicionando items
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-//O Scaffold representa uma página é o esqueleto da página
-//Dentro dele temos os widgets
+//Classe filho -> métodos criados abaixo do build serão recriados a cada execução fzd o a lista ser recriada
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
       ),
-      body: Container(
-        child: Center(
-          child: Text("data"),
-        ),
+      //ListView.builder renderiza a lista sob demanda itens em tela
+      body: ListView.builder(
+        //acessar a variável items criado na classe pai
+        itemCount: widget.items.length,
+        //Função para desenhar os itens em tela
+        itemBuilder: (BuildContext context, int index) {
+          return Text(widget.items[index].title);
+        },
       ),
     );
   }
